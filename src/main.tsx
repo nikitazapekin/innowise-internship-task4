eb93ab73a90cdd0a5757c130946cabe52a43ce08
Global Name <zapekinnikitamain@gmail.com>
2025-11-30 00:59:01 +0300

    feat(tanstack): Add tanstack

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { routeTree } from "./routeTree.gen";
+
+import "./index.css";
+
+const router = createRouter({ routeTree });
+
+/* declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+ */
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    {/*  <RouterProvider router={router} /> */}
+
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);

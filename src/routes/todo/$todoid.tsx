0bf361de4d7223b3d7370f0b0bb2de139a80087e
Global Name <zapekinnikitamain@gmail.com>
2025-12-01 18:49:57 +0300

    refactor(cicd): Resolve ci cd issue

--- src/routes/todo/$todoid.tsx
+++ src/routes/todo/$todoid.tsx
@@ -1,9 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-type PageParams = {
-  page: number;
-};
-
 async function getToDoId(id: string): Promise<{ title: string }> {
   const data = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
 
@@ -14,22 +10,16 @@ export const Route = createFileRoute("/todo/$todoid")({
   component: RouteComponent,
   loader: ({ params }) => getToDoId(params.todoid),
   errorComponent: ({ error }) => <div>{error.message}</div>,
-  validateSearch: (search: Record<string, unknown>): PageParams => {
-    return {
-      //  page: Number(search?.page ?? 1),
-      page: 1,
-    };
-  },
 });
 
 function RouteComponent() {
   const { todoid } = Route.useParams();
-  const { page } = Route.useSearch();
+
   const data = Route.useLoaderData();
 
   return (
     <div>
-      Hello "/todo/todoid"! {todoid}.... {page} {JSON.stringify(data)}
+      Hello "/todo/todoid"! {todoid}.... {JSON.stringify(data)}
     </div>
   );
 }
